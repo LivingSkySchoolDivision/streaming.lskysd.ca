a106fd7a67ed37b946a7fd33ec3bbb2b1c421f96
Mark Strendin <mark.strendin@gmail.com>
2014-02-03 10:38:20 -0600

    Added an ID generator thats not publically accessible, to make manually adding videos and streams easier while I work on the UI to do this.

---
+++ LSKYStreamingManager/GenerateIDs.aspx.cs
@@ -0,0 +1,22 @@
+﻿using LSKYStreamingCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LSKYStreamingManager
+{
+    public partial class GenerateIDs : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 25; x++)
+            {
+                litIDs.Text += LSKYCommon.getNewID(6) + "<BR>";
+            }
+
+        }
+    }
+}
